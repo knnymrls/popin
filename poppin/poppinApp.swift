d0a5ab9b084d1da0a17d9f0551b4401757d601c4
Kenny <kmorales9@huskers.unl.edu>
2026-02-28 21:10:47 -0600

    Initial Commit

---
+++ poppin/poppinApp.swift
@@ -0,0 +1,17 @@
+//
+//  poppinApp.swift
+//  poppin
+//
+//  Created by Kenny Morales on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct poppinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
